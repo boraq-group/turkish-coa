7658a300b71a72fd20292dbc963c3e42902729ac
101t <tarek.it.eng@gmail.com>
2020-03-02 11:41:03 +0300

    new updates

--- turkish_coa/__manifest__.py
+++ turkish_coa/__manifest__.py
@@ -2,25 +2,31 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Turkish Accounting',
+    'name': 'Turkey - Accounting',
     'version': '1.0',
     'category': 'Localization',
     'description': """
-    CoA Turkish, Account Group, Formal Chart of Account for Turkey with Account groups and default journals with defaults accounts for the journals.
+Turkey Localization
+==========================================================
+
+    Turkey Localization 
+    Create account and account group
     """,
-    'summary': 'CoA Turkish, Formal Chart of Account for Turkey with Account groups and default journals with defaults accounts for the journals.',
-    'author': 'Boraq-Group',
-    'maintainer':'https://boraq-group.com',
+    'author': 'boraq',
+    'maintainer':'www.boraq.com',
     'depends': ['account'],
     'data': [
         'data/l10n_tr_chart_data.xml',
+        'data/account.group.csv',
         'data/account.account.template.csv',
         'data/l10n_tr_chart_post_data.xml',
         'data/account_data.xml',
         'data/account_tax_template_data.xml',
-        'data/account.group.csv',
         'data/account_chart_template_data.xml',
+        "data/account_group_parent.xml",
+        "data/account_account_group.xml",
     ],
+    
     'installable': True,
     'application': True,
     'auto_install': True,
